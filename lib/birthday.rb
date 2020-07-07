f08e1f40b44c42a25b985e75cd5ed8ca3231d2c6
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-07 13:15:26 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,5 +6,5 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here
-  return "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
